1c93ba0c275601ab125347029b37737b501bd489
kyungmin0729 <kyungmin0729@naver.com>
2023-12-06 00:14:28 +0900

    chatting v2 front

--- port2023-chatting_be/port2023-chatting_fe/src/outlines/Header.js
+++ port2023-chatting_be/port2023-chatting_fe/src/outlines/Header.js
@@ -1,50 +1,64 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { MdHome } from 'react-icons/md';
+import { FiLogIn } from 'react-icons/fi';
 import fontSizes from '../styles/fontSizes';
 
 const OuterBox = styled.header`
-    display: flex;
-    justify-content: space-between;
-    background: #000;
+  display: flex;
+  justify-content: space-between;
+  background: #000;
+  color: #fff;
+  height: 50px;
+  align-items: center;
+  padding: 0 15px;
+  * {
     color: #fff;
-    height: 50px;
-    align-items: center;
-    padding: 0 15px;
-    * {
-        color: #fff;
-        font-size: ${props => props.fontSize ? fontSizes[props.fontSize] : '1rem'};
+    line-hight: 10%,
+    font-size: ${(props) =>
+      props.fontSize ? fontSizes[props.fontSize] : '1rem'};
+  }
+  .startbtn {
+    svg {
+        vertical-align: middle;
+        margin-right: 2px;
+        margin-bottom: 2px;
     }
-    .btns {
-        a {
-            border: 1px solid #fff;
-            border-radius: 5px; 
-            padding: 5px 10px;
-            transition: all 0.5s;
-        }
-        a+a { 
-            margin-left: 5px; 
-        }
-        a:hover {
-            background: #fff;
-            color: #000;
-        }
+    a:hover {
+        
     }
-    
+  }
+  .btns {
+    a {
+      border: 1px solid #fff;
+      border-radius: 5px;
+      padding: 5px 10px;
+      transition: all 0.5s;
+    }
+    a + a {
+      margin-left: 5px;
+    }
+    a:hover {
+      background: #fff;
+      color: #000;
+    }
+  }
 `;
 
 const Header = () => {
-    return (
-        <OuterBox fontSize="medium">
-            <Link to="/"><MdHome /></Link>
-            
-            <div className="btns">
-                <Link to="/room">방 목록</Link>
-                <Link to="/join">회원가입</Link>
-                <Link to="/login">로그인</Link>
-            </div>
-        </OuterBox>
-    );
+  return (
+    <OuterBox fontSize="medium">
+      <div className="startbtn">
+        <FiLogIn />
+        <Link to="/">시작하기</Link>
+      </div>
+
+      <div className="btns">
+        <Link to="/room">방 목록</Link>
+        <Link to="/join">회원가입</Link>
+        <Link to="/login">로그인</Link>
+      </div>
+    </OuterBox>
+  );
 };
 
 export default Header;
